d0360e8f7d297db3887233d8f54a4f97f4b7c452
John Chen <john@soobb.com>
2024-04-26 12:49:15 -0500

    Change linkage to `maximum` to avoid over-merging

--- analysis/playground-c1.ts
+++ analysis/playground-c1.ts
@@ -4,8 +4,6 @@ import { LLMName } from '../utils/llms.js';
 import { Consolidator1 } from './codebooks/consolidator-1.js';
 import { InitializeEmbeddings, Model } from '../utils/embeddings.js';
 
-InitializeEmbeddings("gecko-768-similarity");
-
 /*
 import { loadEvaluator } from "langchain/evaluation";
 const chain = await loadEvaluator("embedding_distance", { embedding: Model });
@@ -18,8 +16,9 @@ const res = await chain.evaluateStrings({
 console.log({ res });*/
 
 await UseLLMs(async () => {
+    InitializeEmbeddings("gecko-768-similarity");
     await ConsolidateConversations(new Consolidator1(), "Users of Physics Lab (Group 1)", "0~17-gpt-3.5-turbo.json", "low-level-3", LLMName, false);
     // await ConsolidateConversations(new Consolidator1(), "Users of Physics Lab (Group 2)", "0~16-gpt-3.5-turbo.json", "low-level-3", LLMName, false);
-}, "llama3-70b"); // llama3-70b
+}, "gpt-3.5-turbo", "llama3-70b"); // llama3-70b
 
 process.exit(0);
