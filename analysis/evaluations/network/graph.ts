878893cfb6076eeb4854270d3c379e346b270075
John Chen <john@soobb.com>
2024-05-24 00:25:29 -0500

    Switch to inverse lerp for clustering

--- analysis/evaluations/network/graph.ts
+++ analysis/evaluations/network/graph.ts
@@ -67,10 +67,15 @@ export function BuildSemanticGraph(Dataset: CodebookComparison, Parameter: Param
     Graph.Components = FindCommunities(Graph.Nodes, Graph.Links, (Node, Links) => {
         return Node.Data.Examples?.length ?? 0 + 
             2 * (Node.NearOwners?.size ?? 0) + 
-            2 * Links.reduce((Sum, Link) => Sum + Math.pow(Parameter.LinkMaximumDistance - Link.Distance, 2), 0) + 
+            2 * Links.reduce((Sum, Link) => Sum + Math.pow(1 - InverseLerp(Parameter.LinkMinimumDistance, Parameter.LinkMaximumDistance, Link.Distance), 3), 0) + 
             0.2 * Node.Data.Label.length;
-    }, (Link) => 2 * Math.pow(Parameter.LinkMaximumDistance - Link.Distance, 2));
+    }, (Link) => Math.pow(1 - InverseLerp(Parameter.LinkMinimumDistance, Parameter.LinkMaximumDistance, Link.Distance), 3));
     return Graph;
+}
+
+/** Lerp: Linearly interpolate between two values. */
+export function InverseLerp(a: number, b: number, t: number): number {
+    return (t - a) / (b - a);
 }
 
 /** IdentifyComponents: Identify the connected components in the graph. */
