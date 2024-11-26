47fc6c32e18a00a3e5e5a218a7efb7d8d9cd615f
Frank <25704248+rakuzen25@users.noreply.github.com>
2024-11-25 22:23:15 -0600

    chore: update eslint config and add prettier

--- eslint.config.js
+++ eslint.config.js
@@ -3,11 +3,13 @@
 import eslint from "@eslint/js";
 import { includeIgnoreFile } from "@eslint/compat";
 import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 export default tseslint.config(
     eslint.configs.recommended,
     tseslint.configs.strictTypeChecked,
     tseslint.configs.stylisticTypeChecked,
+    eslintConfigPrettier,
     includeIgnoreFile(`${import.meta.dirname}/.gitignore`),
     {
         languageOptions: {
@@ -29,6 +31,46 @@ export default tseslint.config(
                     destructuredArrayIgnorePattern: "^_",
                     varsIgnorePattern: "^_",
                     ignoreRestSiblings: true,
+                },
+            ],
+            "@typescript-eslint/restrict-template-expressions": ["error", {}],
+            curly: "error",
+            "dot-notation": "error",
+            eqeqeq: "error",
+            "no-else-return": "error",
+            "no-empty": [
+                "error",
+                {
+                    allowEmptyCatch: true,
+                },
+            ],
+            "no-extra-bind": "error",
+            "no-labels": "error",
+            "no-lone-blocks": "error",
+            "no-loop-func": "error",
+            "no-new-func": "error",
+            "no-new-object": "error",
+            "no-new-wrappers": "error",
+            "no-redeclare": "error",
+            "no-template-curly-in-string": "error",
+            "no-unreachable": "error",
+            "no-useless-constructor": "error",
+            "prefer-arrow-callback": "error",
+            "prefer-exponentiation-operator": "error",
+            "prefer-template": "error",
+            quotes: [
+                "error",
+                "double",
+                {
+                    avoidEscape: true,
+                },
+            ],
+            "require-await": "error",
+            "sort-imports": [
+                "error",
+                {
+                    ignoreCase: true,
+                    ignoreDeclarationSort: true,
                 },
             ],
         },
