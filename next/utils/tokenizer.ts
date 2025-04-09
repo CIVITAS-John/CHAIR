71e9a6cc7807359aa0cb55c3d9c707cb687323c7
Frank <25704248+rakuzen25@users.noreply.github.com>
2025-04-08 22:32:39 -0500

    chore: remove next folder

--- next/utils/tokenizer.ts
+++
@@ -1,9 +0,0 @@
-import { encoding_for_model } from "@dqbd/tiktoken";
-
-// This utils allows for estimating tokens for a given string.
-// We don't need much accuracy, so we use 3.5-turbo/4 tokenizer for everything
-const encoding = encoding_for_model("gpt-3.5-turbo");
-
-// Get the tokens from a string.
-export const tokenize = (text: string) =>
-    typeof text.charCodeAt !== "function" ? new Uint32Array(0) : encoding.encode(text);
